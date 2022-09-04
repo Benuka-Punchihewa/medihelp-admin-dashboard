c5cecc36665f70b611e2cc4c0508df452b0b7fd4
BeastMaster99 <it20066048@my.sliit.lk>
2022-09-04 20:12:33 +0530

    fixed side bar layout issues

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 import React from "react";
-import { Grid ,Stack } from "@mui/material";
+import { Box, Grid, Stack } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/common/SideBar";
 
 const App = () => {
   return (
     <React.Fragment>
-    <Stack flexDirection="row">
-      <Sidebar/>
-
-      <Grid container spacing={3}>
-        <Grid item xs={12} >
-        
-        </Grid>
-        <Grid item xs={12}>
-          body
-          {/* top navigation bar comes here */}
-          {/* views */}
-
-          <BrowserRouter>
-            <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
-          </BrowserRouter>
-          
-        </Grid>
-      </Grid>
-
-      
-    </Stack>
-
-    
-     
-
+      <Stack flexDirection="row">
+        <Box sx={{ width: 250 }}>
+          <Sidebar />
+        </Box>
+        <Box>
+          <Grid container spacing={3}>
+            <Grid item xs={12}></Grid>
+            <Grid item xs={12}>
+              body
+              {/* top navigation bar comes here */}
+              {/* views */}
+              <BrowserRouter>
+                <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+              </BrowserRouter>
+            </Grid>
+          </Grid>
+        </Box>
+      </Stack>
     </React.Fragment>
   );
 };
--- src/components/common/SideBar.jsx
+++ src/components/common/SideBar.jsx
@@ -1,111 +1,115 @@
-
 import React from "react";
 import { Grid, Stack, Typography } from "@mui/material";
 import {
-    Dashboard,
-    Medication,
-    ShoppingBag,
-    Payment,
-    Vaccines,
-    Logout,
-    Person,
-  } from "@mui/icons-material";
+  Dashboard,
+  Medication,
+  ShoppingBag,
+  Payment,
+  Vaccines,
+  Logout,
+  Person,
+} from "@mui/icons-material";
 
-  
-  import {
-    Box,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-  } from "@mui/material";
-import colors from "../../assets/styles/colors";  
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import colors from "../../assets/styles/colors";
 
-  
-  const Sidebar = ({mode,setMode}) => {
-    return (
-        <Box position="fixed" 
-            sx={{ display: { xs: "none", sm: "block" ,
-                background: colors.primary,
-                color:colors.white,
-                height: '100%' ,width:250 } }}>
-          
-          <Grid item xs={8}>
-            <Stack
-              direction="row"
-              spacing={5}
-              justifyContent="flex-start"
-              alignItems="righ"
-              sx={{ height: "100%" }}
-            >
-              <Typography
-                variant="h4"
-                sx={{ fontWeight: "bold", color: colors.white }}
-              >
-                MediHelp
-              </Typography>
-            </Stack>
-          </Grid>
-          
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="/dashboard">
-                <ListItemIcon sx={{ display: { color:colors.white} }}>
-                  <Dashboard />
-                </ListItemIcon>
-                <ListItemText primary="Dashboard" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="#simple-list">
-                <ListItemIcon sx={{ display: { color:colors.white} }}>
-                    <Person />
-                </ListItemIcon>
-                <ListItemText primary="Users" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="/pharmacy">
-                <ListItemIcon sx={{ display: { color:colors.white} }}>
-                  <Medication/>
-                </ListItemIcon>
-                <ListItemText primary="Pharmacies" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="#simple-list">
-                <ListItemIcon sx={{ display: { color:colors.white} }}>
-                  <ShoppingBag />
-                </ListItemIcon >
-                <ListItemText primary="Orders" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="#simple-list">
-                <ListItemIcon sx={{ display: { color:colors.white} }}>
-                  <Vaccines />
-                </ListItemIcon>
-                <ListItemText primary="Medicines" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="#simple-list">
-                <ListItemIcon sx={{ display: { color:colors.white} }}>
-                  <Payment />
-                </ListItemIcon>
-                <ListItemText primary="Payments" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="#simple-list">
-                <ListItemIcon sx={{ display: { color:colors.white} }}>
-                  <Logout />
-                </ListItemIcon>
-                <ListItemText primary="Logout" />
-              </ListItemButton>
-            </ListItem>
-            {/* <ListItem disablePadding>
+const Sidebar = () => {
+  return (
+    <Box
+      position="fixed"
+      sx={{
+        display: {
+          xs: "none",
+          sm: "block",
+          background: colors.primary,
+          color: colors.white,
+          height: "100%",
+          width: 250,
+        },
+      }}
+    >
+      <Grid item xs={8}>
+        <Stack
+          direction="row"
+          spacing={5}
+          justifyContent="flex-start"
+          alignItems="righ"
+          sx={{ height: "100%" }}
+        >
+          <Typography
+            variant="h4"
+            sx={{ fontWeight: "bold", color: colors.white }}
+          >
+            MediHelp
+          </Typography>
+        </Stack>
+      </Grid>
+
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="/dashboard">
+            <ListItemIcon sx={{ display: { color: colors.white } }}>
+              <Dashboard />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon sx={{ display: { color: colors.white } }}>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary="Users" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="/pharmacy">
+            <ListItemIcon sx={{ display: { color: colors.white } }}>
+              <Medication />
+            </ListItemIcon>
+            <ListItemText primary="Pharmacies" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon sx={{ display: { color: colors.white } }}>
+              <ShoppingBag />
+            </ListItemIcon>
+            <ListItemText primary="Orders" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon sx={{ display: { color: colors.white } }}>
+              <Vaccines />
+            </ListItemIcon>
+            <ListItemText primary="Medicines" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon sx={{ display: { color: colors.white } }}>
+              <Payment />
+            </ListItemIcon>
+            <ListItemText primary="Payments" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon sx={{ display: { color: colors.white } }}>
+              <Logout />
+            </ListItemIcon>
+            <ListItemText primary="Logout" />
+          </ListItemButton>
+        </ListItem>
+        {/* <ListItem disablePadding>
               <ListItemButton component="a" href="#simple-list">
                 <ListItemIcon>
                   <ModeNight />
@@ -113,11 +117,9 @@ import colors from "../../assets/styles/colors";
                 <Switch onChange={e=>setMode(mode === "light" ? "dark" : "light")}/>
               </ListItemButton>
             </ListItem> */}
-          </List>
-        </Box>
-    
-    );
-  };
-  
-  export default Sidebar;
-  
+      </List>
+    </Box>
+  );
+};
+
+export default Sidebar;
